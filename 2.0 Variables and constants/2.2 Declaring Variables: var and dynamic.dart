c9937edd1effe3ec73bf34c561fad849595087a3
Anufriev Dmitry <skezydeveloper@gmail.com>
2023-10-17 17:03:19 +0300

    2.2 Declaring variables:var and dynamic

---
+++ 2.0 Variables and constants/2.2 Declaring Variables: var and dynamic.dart
@@ -0,0 +1,10 @@
+void main() {
+  dynamic x = 88; // Тип переменной int
+  print('X = $x');
+
+  x = "Four"; // Изменим тип переменной
+  print('X = $x');
+  
+  var str = "hello"; // Тип переменной string
+  var list = [1,2,3]; // Тип переменной List
+}
