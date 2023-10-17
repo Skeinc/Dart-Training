759680c6fe1a20fcbd2dd1b61fa359b376c27635
Anufriev Dmitry <skezydeveloper@gmail.com>
2023-10-17 17:57:15 +0300

    2.4 Late modificator

---
+++ 2.0 Variables and constants/2.4 Late modificator.dart
@@ -0,0 +1,12 @@
+void main() {
+  late String str; // Создание переменной, значение которой будет объявленно позже
+  late final double PI; // Создание константы, значение которой будет объявлено позже
+
+  String getStr() {
+    return str = "Hello, World!";
+  }
+
+  PI = 3.14;
+
+  print('str = ' + getStr() + '\nPI = $PI');
+}
