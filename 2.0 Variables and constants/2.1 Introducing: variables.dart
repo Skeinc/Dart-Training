7f573281b706af9c38ac7a8fa89e5ad4b4355e63
Anufriev Dmitry <skezydeveloper@gmail.com>
2023-10-17 14:59:25 +0300

    2.1 Introducing: variables

---
+++ 2.0 Variables and constants/2.1 Introducing: variables.dart
@@ -0,0 +1,8 @@
+void main() {
+  var name = "Dmitry"; // Создание переменных без указания типы с их инициализацией
+  int age = 20; // Создание переменной с указанием типа переменной
+  dynamic city; // Создание переменной с динамическим типом
+  city = "Nizhny Novgorod";
+
+  print('Name: $name\nAge: $age\nCity: $city');
+}
